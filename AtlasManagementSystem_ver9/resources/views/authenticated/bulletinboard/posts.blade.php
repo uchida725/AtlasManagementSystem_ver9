93964c241b1450794f5bcc4a2a57b46428590997
内田奈津子 <>
2025-04-30 13:40:12 +0900

    no message

--- AtlasManagementSystem_ver9/resources/views/authenticated/bulletinboard/posts.blade.php
+++ AtlasManagementSystem_ver9/resources/views/authenticated/bulletinboard/posts.blade.php
@@ -23,12 +23,17 @@
     </div>
     @endforeach
   </div>
+  <!-- ここから右側の絞り込み検索部分 -->
   <div class="other_area border w-25">
     <div class="border m-4">
       <div class=""><a href="{{ route('post.input') }}">投稿</a></div>
-      <div class="">
-        <input type="text" placeholder="キーワードを検索" name="keyword" form="postSearchRequest">
+      <div class="button_search_area">
+        <form action="{{ route('post.show') }}" method="get" id="postSearchRequest">
+          <input type="text" placeholder="キーワードを検索" name="keyword" form="postSearchRequest">
         <input type="submit" value="検索" form="postSearchRequest">
+
+        </form>
+
       </div>
       <input type="submit" name="like_posts" class="category_btn" value="いいねした投稿" form="postSearchRequest">
       <input type="submit" name="my_posts" class="category_btn" value="自分の投稿" form="postSearchRequest">
@@ -38,7 +43,7 @@
         <li class="main_categories" category_id="{{ $category->id }}"><span>{{ $category->main_category }}<span></li>
         <!-- {{-- サブカテゴリー一覧（クリックで絞り込み） --}} -->
     @foreach($category->subCategories as $sub)
-  <form action="{{ route('post.show') }}" method="GET">
+  <form action="{{ route('post.show') }}" method="get" id="postSearchRequest">
     <input type="hidden" name="sub_category_id" value="{{ $sub->id }}">
     <button type="submit" class="category_btn">{{ $sub->sub_category }}</button>
   </form>

3285ddc2627f4120c435713491628d951b917108
内田奈津子 <>
2025-07-21 22:32:03 +0900

    no message

--- AtlasManagementSystem_ver9/resources/views/authenticated/calendar/general/calendar.blade.php
+++ AtlasManagementSystem_ver9/resources/views/authenticated/calendar/general/calendar.blade.php
@@ -10,7 +10,7 @@
       </div>
     </div>
     <div class="text-right w-75 m-auto">
-      <input type="submit" class="btn btn-primary" value="予約する" form="reserveParts">
+      <input type="submit" class="reserve-btn btn btn-primary" value="予約する" form="reserveParts">
     </div>
   </div>
 </div>
